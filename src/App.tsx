a56653000a4d08642661a195a488f043f71e72f6
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-01-23 20:02:41 +0100

    remove redundant imports and variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { Main } from './components/Main/Main';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const navbar = {
-  OFF: "OFF",
-  ON: "ON"
-}
-
 function App() {
   return (
     <Router>
--- src/components/Navbar/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles/Navbar.module.css';
-import hiddenStyles from '../HiddenNavbar/styles/HiddenNavbar.module.css';
 
 type Props = {
     navbarIsHidden: boolean;
