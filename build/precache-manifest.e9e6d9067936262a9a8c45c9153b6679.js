66a12ebccd02900d67b643b112d072edee905363
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-01-23 20:05:49 +0100

    build

---
+++ build/precache-manifest.e9e6d9067936262a9a8c45c9153b6679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4ecafdb876c254720287f7559f845e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69403852a9174e183847",
+    "url": "/static/css/main.76458433.chunk.css"
+  },
+  {
+    "revision": "d6fe86c53aa956e2c6f7",
+    "url": "/static/js/2.e980b6af.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e980b6af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69403852a9174e183847",
+    "url": "/static/js/main.53823a19.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
